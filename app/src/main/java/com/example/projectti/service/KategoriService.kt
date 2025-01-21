07a9826df1188c6108b3d77f3a84f3de23c6e7d2
nauvalachid <nauvalachid@gmail.com>
2025-01-22 01:08:41 +0700

    Menambahkan interface KategoriService untuk operasi CRUD pada data kategori

---
+++ app/src/main/java/com/example/projectti/service/KategoriService.kt
@@ -0,0 +1,42 @@
+package com.example.projectti.service
+
+import com.example.projectti.model.Kategori
+import com.example.projectti.model.Penerbit
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+interface KategoriService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json"
+    )
+
+    // Mengambil semua data kategori
+    @GET("kategori/readkategori.php")
+    suspend fun getKategori(): List<Kategori>
+
+    // Mengambil satu data kategori berdasarkan id_kategori
+    @GET("kategori/read1kategori.php")
+    suspend fun getKategoriById(@Query("id_kategori") idKategori: String): Kategori
+
+    // Menambahkan data kategori baru
+    @POST("kategori/createkategori.php")
+    suspend fun insertKategori(@Body kategori: Kategori): Response<Void>
+
+    // Mengupdate data kategori berdasarkan id_kategori
+    @PUT("kategori/editkategori.php")
+    suspend fun updateKategori(
+        @Query("id_kategori") idKategori: String,
+        @Body kategori: Kategori
+    ): Response<Void>
+
+    // Menghapus data kategori berdasarkan id_kategori
+    @DELETE("kategori/deletekategori.php")
+    suspend fun deleteKategori(@Query("id_kategori") idKategori: String): Response<Void>
+}
