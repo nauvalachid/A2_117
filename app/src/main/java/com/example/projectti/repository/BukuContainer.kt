8532f724d3bf00538be3bdbc580c837f0e6fbac5
nauvalachid <nauvalachid@gmail.com>
2025-01-23 15:45:56 +0700

    Implementasi NetworkKategoriRepository dengan fungsi CRUD lengkap untuk Kategori

---
+++ app/src/main/java/com/example/projectti/repository/BukuContainer.kt
@@ -0,0 +1,39 @@
+package com.example.projectti.repository
+
+import com.example.projectti.model.Penerbit
+import com.example.projectti.model.Penulis
+import com.example.projectti.service.BukuService
+import com.example.projectti.service.KategoriService
+import com.example.projectti.service.PenerbitService
+import com.example.projectti.service.PenulisService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainer {
+    val bukuRepository : BukuRepository
+    val kategoriRepository : KategoriRepository
+    val penerbitRepository : PenerbitRepository
+    val penulisRepository : PenulisRepository
+}
+
+class BukuContainer : AppContainer {
+    private val baseUrl = "http://10.0.2.2/PROJECTTI/" //localhost diganti ip kalo run di hp
+    private val json = Json { ignoreUnknownKeys = true }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val bukuService: BukuService by lazy {retrofit.create(BukuService::class.java)}
+    override val bukuRepository: BukuRepository by lazy {NetworkBukuRepository(bukuService)}
+
+    private val kategoriService: KategoriService by lazy {retrofit.create(KategoriService::class.java)}
+    override val kategoriRepository: KategoriRepository by lazy {NetworkKategoriRepository(kategoriService)}
+
+    private val penerbitService: PenerbitService by lazy {retrofit.create(PenerbitService::class.java)}
+    override val penerbitRepository: PenerbitRepository by lazy {NetworkPenerbitRepository(penerbitService)}
+
+    private val penulisService: PenulisService by lazy {retrofit.create(PenulisService::class.java)}
+    override val penulisRepository: PenulisRepository by lazy {NetworkPenulisRepository(penulisService)}
+}
---
+++ app/src/main/java/com/example/projectti/repository/BukuRepository.kt
@@ -0,0 +1,53 @@
+package com.example.projectti.repository
+
+import com.example.projectti.model.Buku
+import com.example.projectti.service.BukuService
+import okio.IOException
+
+// Interface Repository
+interface BukuRepository {
+    suspend fun getBuku(): List<Buku>
+    suspend fun insertBuku(buku: Buku)
+    suspend fun updateBuku(idBuku: Int, buku: Buku)
+    suspend fun deleteBuku(idBuku: Int)
+    suspend fun getBukuById(idBuku: Int): Buku
+}
+
+// Implementasi Repository
+class NetworkBukuRepository(
+    private val bukuApiService: BukuService
+) : BukuRepository {
+    override suspend fun getBuku(): List<Buku> =
+        bukuApiService.getBuku()
+
+    override suspend fun insertBuku(buku: Buku) {
+        val response = bukuApiService.insertBuku(buku)
+        if (!response.isSuccessful) {
+            throw IOException("Failed to insert buku. HTTP Status code: ${response.code()}")
+        }
+    }
+
+    override suspend fun updateBuku(idBuku: String, buku: Buku) {
+        val response = bukuApiService.updateBuku(idBuku, buku)
+        if (!response.isSuccessful) {
+            throw IOException("Failed to update buku. HTTP Status code: ${response.code()}")
+        }
+    }
+
+    override suspend fun deleteBuku(idBuku: String) {
+        try {
+            val response = bukuApiService.deleteBuku(idBuku)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete buku. HTTP Status code: ${response.code()}")
+            } else {
+                println("Success: ${response.message()}")
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getBukuById(idBuku: String): Buku {
+        return bukuApiService.getBukuById(idBuku)
+    }
+}
