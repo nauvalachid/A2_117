8cbc81083cf9596eec1b4e489d05ec0e3c585bce
nauvalachid <nauvalachid@gmail.com>
2025-01-28 22:34:42 +0700

    mengubah menjadi "pengelolaHalaman"

--- app/src/main/java/com/example/projectti/MainActivity.kt
+++ app/src/main/java/com/example/projectti/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.projectti.Navigation.PengelolaHalaman
 import com.example.projectti.ui.theme.ProjectTITheme
+import com.example.projectti.ui.view.Penulis.HomeScreenPenulis
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +22,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ProjectTITheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PengelolaHalaman(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
