323cf26515ab31244d1505ebc012fb5605a9ea00
nauvalachid <nauvalachid@gmail.com>
2025-01-28 21:23:09 +0700

    memindahkan BukuContainer.kt yg awalnya di package repository dipindah ke package DependencitionInjection

--- app/src/main/java/com/example/projectti/DependencitionInjection/BukuContainer.kt
+++ app/src/main/java/com/example/projectti/DependencitionInjection/BukuContainer.kt
@@ -1,7 +1,13 @@
-package com.example.projectti.repository
+package com.example.projectti.DependencitionInjection
 
-import com.example.projectti.model.Penerbit
-import com.example.projectti.model.Penulis
+import com.example.projectti.repository.BukuRepository
+import com.example.projectti.repository.KategoriRepository
+import com.example.projectti.repository.NetworkBukuRepository
+import com.example.projectti.repository.NetworkKategoriRepository
+import com.example.projectti.repository.NetworkPenerbitRepository
+import com.example.projectti.repository.NetworkPenulisRepository
+import com.example.projectti.repository.PenerbitRepository
+import com.example.projectti.repository.PenulisRepository
 import com.example.projectti.service.BukuService
 import com.example.projectti.service.KategoriService
 import com.example.projectti.service.PenerbitService
@@ -26,14 +32,14 @@ class BukuContainer : AppContainer {
         .baseUrl(baseUrl).build()
 
     private val bukuService: BukuService by lazy {retrofit.create(BukuService::class.java)}
-    override val bukuRepository: BukuRepository by lazy {NetworkBukuRepository(bukuService)}
+    override val bukuRepository: BukuRepository by lazy { NetworkBukuRepository(bukuService) }
 
     private val kategoriService: KategoriService by lazy {retrofit.create(KategoriService::class.java)}
-    override val kategoriRepository: KategoriRepository by lazy {NetworkKategoriRepository(kategoriService)}
+    override val kategoriRepository: KategoriRepository by lazy { NetworkKategoriRepository(kategoriService) }
 
     private val penerbitService: PenerbitService by lazy {retrofit.create(PenerbitService::class.java)}
-    override val penerbitRepository: PenerbitRepository by lazy {NetworkPenerbitRepository(penerbitService)}
+    override val penerbitRepository: PenerbitRepository by lazy { NetworkPenerbitRepository(penerbitService) }
 
     private val penulisService: PenulisService by lazy {retrofit.create(PenulisService::class.java)}
-    override val penulisRepository: PenulisRepository by lazy {NetworkPenulisRepository(penulisService)}
+    override val penulisRepository: PenulisRepository by lazy { NetworkPenulisRepository(penulisService) }
 }
