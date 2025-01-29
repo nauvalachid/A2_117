817d60b5a1066567596cf82f8443fd869450d8cc
nauvalachid <nauvalachid@gmail.com>
2025-01-29 09:47:59 +0700

    menambahkan halaman detail & update

--- app/src/main/java/com/example/projectti/ui/viewmodel/Buku/PenyediaBukuViewModel.kt
+++ app/src/main/java/com/example/projectti/ui/viewmodel/Buku/PenyediaBukuViewModel.kt
@@ -5,11 +5,24 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.projectti.ui.viewmodel.Kategori.DetailKategoriViewModel
+import com.example.projectti.ui.viewmodel.Kategori.UpdateKategoriViewModel
+import com.example.projectti.ui.viewmodel.Penulis.aplikasiBuku
 
 object PenyediaBukuViewModel {
     val Factory = viewModelFactory {
         initializer {
             InsertBukuViewModel(
+                aplikasiBuku().container.bukuRepository
+            )
+        }
+        initializer {
+            DetailBukuViewModel(
+                aplikasiBuku().container.bukuRepository
+            )
+        }
+        initializer {
+            UpdateBukuViewModel(
                 aplikasiBuku().container.bukuRepository
             )
         }
